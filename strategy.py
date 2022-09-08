ade20d8f8f8c62b42f9214b4caea1a20d5990f88
achatur18 <abhay.chaturvedi@awone.ai>
2022-09-08 21:14:18 +0530

    taking last second cadle

--- strategy.py
+++ strategy.py
@@ -20,18 +20,18 @@ def Strategy_1_5_15(client, symbol='BTCUSDT', flag=False):
     candles = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_1MINUTE)
     df_1=pd.DataFrame(candles, columns=cols)
     flag_1=Supertrend(df_1, ewm=False)
-    flag_1=flag_1['Supertrend'][len(flag_1)-1]
+    flag_1=flag_1['Supertrend'][len(flag_1)-2]
 
 
     candles = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_5MINUTE)
     df_5=pd.DataFrame(candles, columns=cols)
     flag_5=Supertrend(df_5, ewm=False)
-    flag_5=flag_5['Supertrend'][len(flag_5)-1]
+    flag_5=flag_5['Supertrend'][len(flag_5)-2]
 
     candles = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_15MINUTE)
     df_15=pd.DataFrame(candles, columns=cols)
     flag_15=Supertrend(df_15, ewm=False)
-    flag_15=flag_15['Supertrend'][len(flag_15)-1]
+    flag_15=flag_15['Supertrend'][len(flag_15)-2]
 
     if flag:
         return flag_1, flag_5,  flag_15
@@ -43,12 +43,12 @@ def Strategy_5_15(client, symbol='BTCUSDT', flag=False):
     candles = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_5MINUTE)
     df_5=pd.DataFrame(candles, columns=cols)
     flag_5=Supertrend(df_5, ewm=False)
-    flag_5=flag_5['Supertrend'][len(flag_5)-1]
+    flag_5=flag_5['Supertrend'][len(flag_5)-2]
 
     candles = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_15MINUTE)
     df_15=pd.DataFrame(candles, columns=cols)
     flag_15=Supertrend(df_15, ewm=False)
-    flag_15=flag_15['Supertrend'][len(flag_15)-1]
+    flag_15=flag_15['Supertrend'][len(flag_15)-2]
 
     if flag:
         return flag_5,  flag_15
