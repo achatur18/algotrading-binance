084f46ac8269b08802073f2d2212cfd17eddf067
achatur18 <abhay.chaturvedi@awone.ai>
2022-09-09 20:26:12 +0530

    removed illegal lows

--- strategy.py
+++ strategy.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from supertrend import Supertrend
 from binance import Client
-
+from utils import filter_df
 
 cols=["Kline open time",
 "Open price",
@@ -19,17 +19,20 @@ cols=["Kline open time",
 def Strategy_1_5_15(client, symbol='BTCUSDT', flag=False):
     candles = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_1MINUTE)
     df_1=pd.DataFrame(candles, columns=cols)
+    df_1=filter_df(df_1, 100)
     flag_1=Supertrend(df_1, ewm=False)
     flag_1=flag_1['Supertrend'][len(flag_1)-2]
 
 
     candles = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_5MINUTE)
     df_5=pd.DataFrame(candles, columns=cols)
+    df_5=filter_df(df_5, 200)
     flag_5=Supertrend(df_5, ewm=False)
     flag_5=flag_5['Supertrend'][len(flag_5)-2]
 
     candles = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_15MINUTE)
     df_15=pd.DataFrame(candles, columns=cols)
+    flag_15=filter_df(flag_15, 500)
     flag_15=Supertrend(df_15, ewm=False)
     flag_15=flag_15['Supertrend'][len(flag_15)-2]
 
@@ -42,11 +45,13 @@ def Strategy_1_5_15(client, symbol='BTCUSDT', flag=False):
 def Strategy_5_15(client, symbol='BTCUSDT', flag=False):
     candles = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_5MINUTE)
     df_5=pd.DataFrame(candles, columns=cols)
+    df_5=filter_df(df_5, 100)
     flag_5=Supertrend(df_5, ewm=False)
     flag_5=flag_5['Supertrend'][len(flag_5)-2]
 
     candles = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_15MINUTE)
     df_15=pd.DataFrame(candles, columns=cols)
+    df_15=filter_df(df_15, 100)
     flag_15=Supertrend(df_15, ewm=False)
     flag_15=flag_15['Supertrend'][len(flag_15)-2]
 
--- utils.py
+++ utils.py
@@ -1,5 +1,25 @@
+import pandas as pd
+
 def get_balance(client, symbol):
     return float(round(float(client.get_asset_balance(asset=symbol).get('free')), 8))
 
 def get_price(client, symbol):
     return float(round(float(client.get_symbol_ticker(symbol=symbol)['price']), 8))
+
+
+def filter_df(df, true_range_threshold=200):
+    high = df['High'].apply(float)
+    low = df['Low'].apply(float)
+    close = df['Close'].apply(float)
+
+    # calculate ATR
+    price_diffs = [high - low, 
+                    high - close.shift(), 
+                    close.shift() - low]
+
+    true_range = pd.concat(price_diffs, axis=1)
+    true_range = true_range.abs().max(axis=1)
+    
+    df=df[true_range<true_range_threshold]
+    df.index=range(0, len(df))
+    return df
