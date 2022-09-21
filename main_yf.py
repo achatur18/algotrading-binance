d7d33a4d050e00e42696c3a22ba0fd46cce2f193
achatur18 <abhay.chaturvedi@awone.ai>
2022-09-21 12:39:56 +0530

    added exceotion for data dowbload

--- main_yf.py
+++ main_yf.py
@@ -29,7 +29,10 @@ while(True):
             flags = Strategy_1_5_15_yf(flag=[True,True,True], index=-1)
         except:
             pass
-        
+        print("Data fetching failed!!! ... Retrying again in 5 secs.")
+        time.sleep(5)
+
+
     StrategyFlag = flags[0]
     for signal in flags:
         StrategyFlag=StrategyFlag and signal
